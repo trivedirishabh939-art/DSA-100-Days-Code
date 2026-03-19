9dfd64185a5dc53c76a1d4818bfd8ce538560daf
Rishah Trivedi <trivedirishabh939@gmail.com>
2026-03-19 08:08:06 +0530

    Add files via upload

---
+++ day-47.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of tree node
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// Create new node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build tree from level order input
+struct TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct TreeNode* root = newNode(arr[0]);
+
+    struct TreeNode* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        struct TreeNode* current = queue[front++];
+
+        // Left child
+        if (i < n && arr[i] != -1) {
+            current->left = newNode(arr[i]);
+            queue[rear++] = current->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            current->right = newNode(arr[i]);
+            queue[rear++] = current->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Function to calculate height
+int height(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct TreeNode* root = buildTree(arr, n);
+
+    printf("%d\n", height(root));
+
+    return 0;
+}
