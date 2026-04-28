b70fcbc638870b9376ee885bbb03ecdf2c1f036e
Rishah Trivedi <trivedirishabh939@gmail.com>
2026-04-28 07:14:59 +0530

    Add files via upload

---
+++ day-87.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if target is at mid
+        if (arr[mid] == target) {
+            return mid;
+        }
+        // If target is greater, ignore left half
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        // If target is smaller, ignore right half
+        else {
+            right = mid - 1;
+        }
+    }
+
+    // Target not found
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
